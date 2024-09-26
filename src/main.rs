28ec79c25358156303da5c1139381b00666b4196
Kristof Kovacs <kristof.kovacs1996@gmail.com>
2024-09-26 20:36:28 +0200

    generating a secret number

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,26 @@
+// Let Rust know that we use an external dependency
+extern crate rand;
+
 // The io library comes from the standard library (a.k.a std)
 // The `use` statement brings the type into scope
 use std::io;
+// Bringing a type called `std::cmp::Ordering` into scope. `Ordering` is an enum
+// which has the variants of `Less`, `Greater`, and `Equal`.
+use std::cmp::Ordering;
+// Put the `Rng` trait in scope for us to use its methods
+use rand::Rng;
 
 // The `main` function is the entry point to the program
 fn main() {
     // The `println!` macro prints a string to the screen
     println!("Guess the number!");
+
+    // The `rand::thread_rng` function will give us the particular random number generator
+    // that we're going to use. We call the `gen_range` method on the random number generator
+    // which takes a range and returns a random number from the range.
+    let secret_number = rand::thread_rng().gen_range(1..101);
+    println!("The secret number is: {}", secret_number);
+
     println!("Please input your guess.");
 
     // The 'let` statement is used to create a variable
@@ -26,5 +41,20 @@ fn main() {
     // The `expect` method will cause the program to crash and display the message
     // passed as an argument, when an error occurs in the `read_line` method.
 
+    // Convert the `String` into a real number type, so we can compare it numerically.
+    // We use the same variable name to `shadow` the previous value with a new one.
+    let guess: u32 = guess.trim().parse().expect("Please type a number!");
+    // If `parse` returns an `Err` `Result` variant, the `expect` call will crash the game
+    // and print the message. If `parse` can successfully convert the string to a number,
+    // it will return the `Ok` variant of `Result`, and `expect` will return the number
+    // that we want from the `Ok` value
+
     println!("You guessed: {}", guess);
+
+    // We use a `match` expression to decide what to do based on the variant of `Ordering`
+    match guess.cmp(&secret_number) {
+        Ordering::Less => println!("Too small!"),
+        Ordering::Greater => println!("Too big!"),
+        Ordering::Equal => println!("You win!"),
+    }
 }
